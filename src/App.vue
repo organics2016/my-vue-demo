104fd40e918c4b793fccf87fdde79c1ebfb1fd02
organics <organics2015@outlook.com>
2019-08-02 19:30:16 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,31 @@
 <template>
-  <div id="app" ref="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <div>
+      <p>
+        If Element is successfully added to this project, you'll see an
+        <code v-text="'<el-button>'"></code>
+        below
+      </p>
+      <el-button>el-button</el-button>
     </div>
-    <router-view/>
   </div>
 </template>
 
 <script>
-import CanvasNest from 'canvas-nest.js'
 
 export default {
-  name: 'app',
-  mounted () {
-    const f = 255
-    const config = {
-      color: Math.round(Math.random() * f) + ',' +
-        Math.round(Math.random() * f) + ',' +
-        Math.round(Math.random() * f),
-      opacity: 1
-    }
-
-    /* eslint-disable no-new */
-    new CanvasNest(this.$refs.app, config)
-  }
+  name: 'app'
 }
 </script>
 
 <style>
-html, body {
-  height: 100%;
-  width: 100%;
-  overflow-y:hidden;
-  overflow-x:hidden;
-}
-
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  height: 100%;
-  width: 100%;
+  margin-top: 60px;
 }
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-  }
 </style>
