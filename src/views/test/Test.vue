c707e9c02fb348ac96f6ae434bf45c139611e49a
organics <organics2015@outlook.com>
2019-12-20 17:12:45 +0800

    update

---
+++ src/views/test/Test.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    testtesttesttesttest
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'test',
+  data: function () {
+    return {
+
+    }
+  },
+  mounted () {
+
+  },
+
+  methods () {
+  }
+}
+</script>
