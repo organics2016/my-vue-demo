6f5f88e1f8e99f7187512ed2a2d6d63849a6a97e
organics <organics2015@outlook.com>
2019-12-24 15:44:12 +0800

    add login page

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -3,7 +3,7 @@
   <div class="limiter">
     <div class="container-login100">
       <div class="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-54">
-        <form class="login100-form validate-form">
+        <div class="login100-form validate-form">
           <span class="login100-form-title p-b-49">
             嘻嘻哈哈
           </span>
@@ -11,13 +11,13 @@
           <div class="wrap-input100 validate-input m-b-23" data-validate="Username is reauired">
             <span class="label-input100">滴滴滴滴滴</span>
             <input class="input100" type="text" name="username" placeholder="Type your username">
-<!--            <span class="focus-input100" data-symbol="&#xf206;"></span>-->
+            <span class="focus-input100"></span>
           </div>
 
           <div class="wrap-input100 validate-input" data-validate="Password is required">
             <span class="label-input100">Password</span>
             <input class="input100" type="password" name="pass" placeholder="Type your password">
-<!--            <span class="focus-input100" data-symbol="&#xf190;"></span>-->
+            <span class="focus-input100"></span>
           </div>
 
           <div class="text-right p-t-8 p-b-31">
@@ -35,7 +35,7 @@
             </div>
           </div>
 
-        </form>
+        </div>
       </div>
     </div>
   </div>
